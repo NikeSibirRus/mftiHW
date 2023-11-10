9fbc387b4979e8be018f77c111e6ee582869821c
NikeSibirRus <nikesibir@yandex.ru>
2023-11-10 14:01:22 +0700

    Initial commit

---
+++ home01/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'home01/index.html')
+
+def aboutus(request):
+    return render(request, 'home01/aboutus.html')
+
+def contacts(request):
+    return render(request, 'home01/contacts.html')
+
+def err404(request, exception):
+    return render(request, 'home01/404.html')
