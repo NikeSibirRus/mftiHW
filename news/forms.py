b10ae5602aeeb31347a34d80ba25c685c59f4756
NikeSibirRus <nikesibir@yandex.ru>
2023-12-11 08:39:35 +0700

    Сборка второй ДЗ

---
+++ news/forms.py
@@ -0,0 +1,38 @@
+from django import forms
+from django.core.validators import MinLengthValidator
+
+from django.forms import ModelForm, Textarea, CheckboxSelectMultiple,Select
+from .models import Article
+
+
+class MultipleFileInput(forms.ClearableFileInput):
+    allow_multiple_selected = True
+
+
+class MultipleFileField(forms.FileField):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault("widget", MultipleFileInput())
+        super().__init__(*args, **kwargs)
+
+    def clean(self, data, initial=None):
+        single_file_clean = super().clean
+        if isinstance(data, (list, tuple)):
+            result = [single_file_clean(d, initial) for d in data]
+        else:
+            result = single_file_clean(data, initial)
+        return result
+
+
+
+class ArticleForm(ModelForm):
+    image_field = MultipleFileField()
+    image_field.label='Список изображений'
+
+    class Meta:
+        model = Article
+        fields = ['title','anouncement','text','tags','category']
+        widgets = {
+            'anouncement': Textarea(attrs={'cols':80,'rows':2}),
+            'text': Textarea(attrs={'cols': 80, 'rows': 6}),
+            'tags': CheckboxSelectMultiple()
+        }
